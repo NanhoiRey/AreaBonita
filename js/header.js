03858b059612757da6d792de3fa8f758645e6107
NanhoiRey <nanhoirey94@gmail.com>
2025-12-11 18:04:35 +0100

    Update2

--- js/header.js
+++ js/header.js
@@ -37,3 +37,12 @@ function initNav() {
   });
 }
 
+
+
+// Cargar footer
+fetch('/components/footer.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  });
+
